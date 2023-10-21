573fbc19ee17ecfe03a3f1ea6b7f494fda3d43a0
rexaulrana <rexaul.rana@gmail.com>
2023-10-21 23:25:07 +0600

    error page and UI update

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -3,7 +3,7 @@ import error from "../../assets/images/404.gif";
 const ErrorPage = () => {
   return (
     <div>
-      <img className="h-screen" src={error} alt="" />
+      <img className="h-screen w-full" src={error} alt="" />
     </div>
   );
 };
